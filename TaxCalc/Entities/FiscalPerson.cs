363f61abc5646d9353c31f0f6ce9cf8d77b5d16f
Lucas Holanda <lucas.c.d.holanda@gmail.com>
2021-11-19 11:01:50 -0300

    Add Full Program
    Added the full and working program. I may modyfy and upgrade it at a later date

---
+++ TaxCalc/Entities/FiscalPerson.cs
@@ -0,0 +1,17 @@
+﻿namespace TaxCalc.Entities {
+    class FiscalPerson : Person {
+        public int Workers { get; private set; }
+
+        public FiscalPerson(string name, double annualGains, int workers) : base(name, annualGains) {
+            Workers = workers;
+        }
+
+        public override double Tax() {
+            if(Workers <= 10) {
+                return AnnualGains * 0.16;
+            } else {
+                return AnnualGains * 0.14;
+            }
+        }
+    }
+}
---
+++ TaxCalc/Entities/NaturalPerson.cs
@@ -0,0 +1,28 @@
+﻿namespace TaxCalc.Entities {
+    class NaturalPerson : Person {
+        public double HealthBills { get; private set; }
+
+        public NaturalPerson(string name, double annualGains, double healthBills) : base(name, annualGains) {
+            HealthBills = healthBills;
+        }
+
+        public override double Tax() {
+            double aux = 0;
+            switch (AnnualGains, HealthBills) {
+                case var e when (AnnualGains < 20000.00 && HealthBills > 0):
+                    aux = (AnnualGains * 0.15) - (HealthBills * 0.5);
+                    break;
+                case var e when (AnnualGains < 20000.00 && HealthBills < 0):
+                    aux = AnnualGains * 0.15;
+                    break;
+                case var e when (AnnualGains > 20000.00 && HealthBills > 0):
+                    aux = (AnnualGains * 0.25) - (HealthBills * 0.5);
+                    break;
+                case var e when (AnnualGains > 20000.00 && HealthBills < 0):
+                    aux = AnnualGains * 0.25;
+                    break;
+            }
+            return aux;
+        }
+    }
+}
